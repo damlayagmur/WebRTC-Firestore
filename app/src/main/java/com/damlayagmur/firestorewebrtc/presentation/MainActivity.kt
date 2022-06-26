cecb03d5e8109bfc7559b0b6cdbe7da86719b7a0
damlayagmr <damlayagmur976@gmail.com>
2022-06-26 23:28:13 +0300

    Update MainActivity

--- app/src/main/java/com/damlayagmur/firestorewebrtc/presentation/MainActivity.kt
+++ app/src/main/java/com/damlayagmur/firestorewebrtc/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.damlayagmur.firestorewebrtc
+package com.damlayagmur.firestorewebrtc.presentation
 
 import android.Manifest
 import android.app.Activity
@@ -8,6 +8,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.damlayagmur.firestorewebrtc.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
